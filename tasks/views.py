57f772c399580cce5a055b74912576d2a2063dce
Tazul Islam <linkedin52425@gmail.com>
2025-01-18 18:28:03 +0600

    my last update done

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from tasks.forms import TaskForm, TaskModelForm
-from tasks.models import Employee, Task
+from tasks.models import Employee, Task, TaskDetail, Project
 from django.contrib import messages
+from datetime import date
+from django.db.models import Q, Count, Max, Min, Avg
+
 
 def manager_dashboard(request):
     return render(request, "dashboard/admin.html")
@@ -31,13 +34,42 @@ def create_task(request):
     return render(request, "task_form.html", context)
 
 def view_task(request):
-    # retrive all data form task
-    tasks = Task.objects.all()
-    #retrive a specific task
-    task3 = Task.objects.get(id = 3)
-    #fetch the first task
-    first_task = Task.objects.first()
-    return render(request, "show_task.html", {"tasks":tasks, "task3":task3, "first_task":first_task})
+
+    # # retrive all data form task
+    # tasks = Task.objects.all()
+    # #retrive a specific task
+    # task3 = Task.objects.get(id = 3)
+    # #fetch the first task
+    # first_task = Task.objects.first()
+
+    #show task that are completed
+    # tasks = Task.objects.filter(status = "COMPLETED") 
+
+    #show the task which due date is today
+    # tasks = Task.objects.filter(due_date = date.today())
+
+    # show the task whose priority is not low
+    # tasks = TaskDetail.objects.exclude(priority = "L")
+
+    # show the task that contain the word 'paper' and status pending
+    #tasks = Task.objects.filter(title__icontains = "c", status = "PENDING")
+
+    #show the task which are pending or in-progress
+    #tasks = Task.objects.filter(Q(status = "PENDING") | Q(status = "IN_PROGRESS"))
+
+    # select related (foreignKey er jonno ek dike sodo khatbe, oneToOneKey er jonno 2 dikei)
+    #tasks = Task.objects.select_related('details').all() 
+    #tasks = TaskDetail.objects.select_related('task').all()
+    #tasks = Task.objects.select_related('project').all()
+
+    #prefetch related(reverse foreign key er jonno kaj kore r manyToMnay er jonno kore)
+    #tasks = Project.objects.prefetch_related('task_set').all()
+    #tasks = Task.objects.prefetch_related('assigned_to').all()
+
+    #Aggregate function
+    #task_count = Task.objects.aggregate(num_task = Count('id'))
+    task_count = Project.objects.annotate(num_task = Count('task')).order_by('num_task')
+    return render(request, "show_task.html", {"task_count":task_count})
 
 
 
